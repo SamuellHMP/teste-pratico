aa447fbd191d5a66a2827a89be3d39047f86a670
SamuellHMP <samuelhmp@hotmail.com>
2025-04-19 10:48:36 -0300

    Ajustar o vite.config.ts para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  base: '/teste-pratico/',
 })
